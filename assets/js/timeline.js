26323f57eb7599accafdacbd6aacedf4fea54a23
Allison Thackston <allison@allisonthackston.com>
2023-11-13 16:51:40 +0000

    Fix timeline anchor to update on resize

--- assets/js/timeline.js
+++ assets/js/timeline.js
@@ -1,12 +1,29 @@
 // Fix timeline anchors
-document.addEventListener("DOMContentLoaded", function() {
-    console.log("Script is running!");
-    // Default topBarHeight to 0 if .topbar doesn't exist
-    var topBarElement = document.querySelector('.topbar');
-    var topBarHeight = topBarElement ? topBarElement.offsetHeight : 0;
-    var timelineTitles = document.querySelectorAll(".TimelineItem-title");
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Timeline script is running!");
 
-    timelineTitles.forEach(function(title) {
-      title.style.top = topBarHeight + "px";
-    });
+  function updateTopBarHeight() {
+      // Recalculate topBarHeight
+      var topBarElement = document.querySelector('.topbar');
+      topBarHeight = topBarElement ? topBarElement.offsetHeight : 0;
+
+      // Update styles for each timeline title
+      timelineTitles.forEach(function (title) {
+          title.style.top = topBarHeight + "px";
+      });
+  }
+
+  // Default topBarHeight to 0 if .topbar doesn't exist
+  var topBarHeight = 0;
+  var timelineTitles = document.querySelectorAll(".TimelineItem-title");
+
+  // Initial update
+  updateTopBarHeight();
+
+  // Add event listener for window resize
+  window.addEventListener('resize', function () {
+      // Call the function to update topBarHeight and adjust styles
+      console.log("Updated height!");
+      updateTopBarHeight();
   });
+});
