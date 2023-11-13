23e1964d96ce4c1d8464749e7be5f99b02569c75
Allison Thackston <allison@allisonthackston.com>
2023-11-13 17:05:18 +0000

    Update anchor link height on resize

--- assets/js/anchor-links.js
+++ assets/js/anchor-links.js
@@ -1,32 +1,47 @@
 // Fix anchor links
 document.addEventListener('DOMContentLoaded', function () {
+    // Function to handle anchor link click
+    function handleAnchorLinkClick(e) {
+        // Prevent the default behavior of the link (e.g., jumping to the anchor)
+        e.preventDefault();
+
+        // Get the target section's ID from the href attribute
+        var targetId = this.getAttribute('href').substring(1);
+
+        // Get the target section by ID
+        var targetSection = document.getElementById(targetId);
+
+        // If the target section exists, calculate the offset based on the top bar's height
+        if (targetSection) {
+            var topBarHeight = document.querySelector('.topbar').offsetHeight;
+            var offsetTop = targetSection.offsetTop - topBarHeight;
+
+            // Scroll to the target section with the calculated offset
+            window.scrollTo({
+                top: offsetTop,
+                behavior: 'smooth' // Optional: Add smooth scrolling effect
+            });
+        }
+    }
+
+    // Function to recalculate topBarHeight on window resize
+    function handleWindowResize() {
+        topBarHeight = document.querySelector('.topbar').offsetHeight;
+    }
+
     // Get all anchor links on the page
     var anchorLinks = document.querySelectorAll('a[href^="#"]');
 
     // Loop through each anchor link
     anchorLinks.forEach(function (link) {
         // Attach a click event listener to each anchor link
-        link.addEventListener('click', function (e) {
-            // Prevent the default behavior of the link (e.g., jumping to the anchor)
-            e.preventDefault();
+        link.addEventListener('click', handleAnchorLinkClick);
+    });
 
-            // Get the target section's ID from the href attribute
-            var targetId = link.getAttribute('href').substring(1);
-
-            // Get the target section by ID
-            var targetSection = document.getElementById(targetId);
-
-            // If the target section exists, calculate the offset based on the top bar's height
-            if (targetSection) {
-                var topBarHeight = document.querySelector('.topbar').offsetHeight;
-                var offsetTop = targetSection.offsetTop - topBarHeight;
-
-                // Scroll to the target section with the calculated offset
-                window.scrollTo({
-                    top: offsetTop,
-                    behavior: 'smooth' // Optional: Add smooth scrolling effect
-                });
-            }
-        });
+    // Add event listener for window resize, debounced for efficiency
+    var resizeTimer;
+    window.addEventListener('resize', function () {
+        clearTimeout(resizeTimer);
+        resizeTimer = setTimeout(handleWindowResize, 250);
     });
 });
