f253be81f4666215cb3eadf5bc79731516b26819
Allison Thackston <allison@allisonthackston.com>
2025-01-27 22:09:03 -0800

    Update devcontainer and vscode settings (#281)
    * Update devcontainer and vscode settings
    
    * Fix test.sh to handle other baseurls

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -5,9 +5,6 @@ ruby_version=$(ruby -v | awk '{print $2}')
 printf %s "$ruby_version" > .ruby-version
 echo "Ruby version $ruby_version set in .ruby-version file"
 
-eval "$(rbenv init - bash)"
-rbenv install
-
 # Install the version of Bundler.
 if [ -f Gemfile.lock ] && grep "BUNDLED WITH" Gemfile.lock >/dev/null; then
     echo "Installing bundler in gemfile"
