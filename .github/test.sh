4ca2c939de989f726b1168b39fe4355ea08ae8a8
Allison Thackston <allison@allisonthackston.com>
2025-01-28 06:07:02 +0000

    Fix test.sh to handle other baseurls

--- .github/test.sh
+++ .github/test.sh
@@ -2,6 +2,6 @@
 set -e
 BASEURL=${BASEURL:-""}
 HOSTURL=${HOSTURL:-""}
-url_swap=".*${HOSTURL}:,^${BASEURL}:"
+url_swap=".*${HOSTURL}${BASEURL}:,^${BASEURL}:"
 
 bundle exec htmlproofer --swap-urls $url_swap _site
